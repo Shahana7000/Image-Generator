3a27b87367eb2d665561fccaf2998263209b540d
Shahana700 <shahanacs2003@gmail.com>
2025-07-21 22:10:34 +0530

    little bit css applied for structure

---
+++ script.js
@@ -0,0 +1,13 @@
+const themeToggleBtn = document.querySelector('.theme-toggle');
+const themeIcon = themeToggleBtn.querySelector('i');
+
+themeToggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    if (document.body.classList.contains('dark-mode')) {
+        themeIcon.classList.remove('fa-moon');
+        themeIcon.classList.add('fa-sun');
+    } else {
+        themeIcon.classList.remove('fa-sun');
+        themeIcon.classList.add('fa-moon');
+    }
+});
